30007d8271d9bb36ee8587da0faaea3b89cd8615
Thejas Hari <107091644+thejas-dev@users.noreply.github.com>
2023-09-21 01:50:08 +0530

    Update VideoComponent.js

--- frontend/e-gazilions-next/components/VideoComponent.js
+++ frontend/e-gazilions-next/components/VideoComponent.js
@@ -10,7 +10,7 @@ export default function VideoComponent(){
             
             
             <div className="d-flex flex-row justify-content-center">
-                <div className="embed-responsive embed-responsive-16by9 flex justify-center">
+                <div className="embed-responsive embed-responsive-16by9 flex justify-center px-5">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/3VkpXLuDmX8?si=l6Mlo6UwTtqLvZeO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>            </div>
                 </div>
             </div> 
