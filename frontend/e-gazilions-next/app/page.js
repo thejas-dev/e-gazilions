7ae6e52d1bbd1120333b3ac6eef3b43cff045f8d
thejas-dev <thejasgamerx123@gmail.com>
2023-09-20 11:10:21 +0530

    make it better

--- frontend/e-gazilions-next/app/page.js
+++ frontend/e-gazilions-next/app/page.js
@@ -1,8 +1,33 @@
+"use client"
+
 import Image from 'next/image'
 import Head from 'next/head';
+import {LiaIndustrySolid} from 'react-icons/lia';
+import {BiRecycle} from 'react-icons/bi';
+import {BsBox} from 'react-icons/bs';
+import {motion} from 'framer-motion';
 
 
 export default function Home() {
+
+
+    const data= [
+    {
+        icon:<LiaIndustrySolid className="h-12 w-12 text-black"/>,
+        name:"Manufature",
+        description:"Manufature the products"
+    },{
+        icon:<BiRecycle className="h-12 w-12 text-black"/>,
+        name:"Recycle",
+        description:"Recycle the products"
+    },{
+        icon:<BsBox className="h-12 w-12 text-black"/>,
+        name:"Reuse",
+        description:"Reuse the products"
+    }   
+    ]
+
+
   return (
     <>
     <Head>
@@ -33,19 +58,50 @@ export default function Home() {
         
         <div id="hero" className="h-[100vh]">
             <div className="bg-image">
-                <div className="card rounded-md bg-white mt-10 p-1" style={{width: "20rem", height: "25rem"}}>
+                <div className="card rounded-md cursor-pointer bg-white mt-10 p-1" style={{width: "20rem", height: "25rem"}}>
                     <img src="https://ik.imagekit.io/d3kzbpbila/thejashari_ikjx3UOmf" className="w-full rounded-t-md " alt="..."/>
                     <div className="card-body p-2">
                         <h5 className="card-title text-2xl">E-Gazilion</h5>
                         <p className="card-text text-lg">"Empowering a sustainable future, one discarded device at a time."</p>
-                        <button className="btn btn-primary bg-blue-600 mt-4 rounded-md text-white px-4 py-3">Know More</button>
+                        <button className="btn btn-primary bg-blue-600 mt-4 rounded-md text-white hover:scale-[105%] transition-all duration-200 
+                        ease-in-out px-4 py-3">Know More</button>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div id="mvv" className="h-[100vh] bg-gray-100 w-full">
-          
+        <div id="mvv" className="min-h-[100vh] flex flex-col items-center 
+        pt-[50px] bg-gray-100 w-full gap-5">
+            <h1 className="text-2xl font-semibold text-gray-900">Product lifecycle</h1>
+            <div className="flex md:flex-row flex-col items-center px-10 justify-between w-full mt-[80px] gap-5">
+                {
+                    data.map((dat,j)=>(
+                        <motion.div 
+                        initial={{
+                            opacity:0,
+                            x:-50
+                        }}
+                        whileInView={{
+                            opacity:1,
+                            x:0
+                        }}
+                        transition={{
+                            duration:0.5
+                        }}
+                        className={`px-5 py-10 flex bg-white rounded-md md:w-[30%] w-[100%] items-center flex-col gap-5`}>
+                            <div className="p-3 rounded-full bg-gray-300/50">
+                            {
+                                dat.icon
+                            }</div> 
+                            <h1 className="text-lg font-semibold text-gray-800">{dat.name}</h1>
+                            <p className="text-md text-center font-semibold text-gray-600 ">{dat.description}</p>
+
+                        </motion.div>
+
+                    ))
+                }
+            </div>
+        
 
         </div>
 
@@ -53,3 +109,22 @@ export default function Home() {
     </>
   )
 }
+
+
+// div>
+//         <div className="d-flex flex-row justify-content-start">
+//             <img src=""/>
+//             <h1>Manufature</h1>
+//             <p></p>
+//         </div>
+//         <div>
+//             <img src=""/>
+//             <h1>Recycle</h1>
+//             <p></p>
+//         </div>
+//         <div>
+//             <img src=""/>
+//             <h1>Reuse</h1>
+//             <p></p>
+//         </div>
+//         </div>
